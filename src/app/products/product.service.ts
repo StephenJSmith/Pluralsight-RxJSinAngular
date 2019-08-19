fce4de73a2dde1abaea7975590bc89d375027bfb
Stephen Smith <stephensmith_aus@yahoo.com.au>
2019-08-20 06:33:26 +1000

    End of Section 6 Mapping Returned Data

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { Observable, throwError } from 'rxjs';
-import { catchError, tap } from 'rxjs/operators';
+import { catchError, tap, map } from 'rxjs/operators';
 
 import { Product } from './product';
 import { SupplierService } from '../suppliers/supplier.service';
@@ -15,6 +15,12 @@ export class ProductService {
 
   products$ = this.http.get<Product[]>(this.productsUrl)
   .pipe(
+    map(products =>
+      products.map(product => ({
+        ...product,
+        price: product.price * 1.5,
+        searchKey: [product.productName]
+      }) as Product)),
     tap(data => console.log('Products: ', JSON.stringify(data))),
     catchError(this.handleError)
   );
