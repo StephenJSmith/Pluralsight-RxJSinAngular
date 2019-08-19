57a2e96278ab36d6149588bc8b4f05e30f929b96
Stephen Smith <stephensmith_aus@yahoo.com.au>
2019-08-20 06:05:28 +1000

    End of Section 5 Going Reactive

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -5,7 +5,6 @@ import { Observable, throwError } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
 
 import { Product } from './product';
-import { Supplier } from '../suppliers/supplier';
 import { SupplierService } from '../suppliers/supplier.service';
 
 @Injectable({
@@ -13,31 +12,15 @@ import { SupplierService } from '../suppliers/supplier.service';
 })
 export class ProductService {
   private productsUrl = 'api/products';
-  private suppliersUrl = this.supplierService.suppliersUrl;
+
+  products$ = this.http.get<Product[]>(this.productsUrl)
+  .pipe(
+    tap(data => console.log('Products: ', JSON.stringify(data))),
+    catchError(this.handleError)
+  );
 
   constructor(private http: HttpClient,
               private supplierService: SupplierService) { }
-
-  getProducts(): Observable<Product[]> {
-    return this.http.get<Product[]>(this.productsUrl)
-      .pipe(
-        tap(data => console.log('Products: ', JSON.stringify(data))),
-        catchError(this.handleError)
-      );
-  }
-
-  private fakeProduct() {
-    return {
-      id: 42,
-      productName: 'Another One',
-      productCode: 'TBX-0042',
-      description: 'Our new product',
-      price: 8.9,
-      categoryId: 3,
-      category: 'Toolbox',
-      quantityInStock: 30
-    };
-  }
 
   private handleError(err: any) {
     // in a real world app, we may send the server to some remote logging infrastructure
